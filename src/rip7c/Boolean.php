56746736b611f1d3ffa49f9553dacd24d3f9d33e
David Herrera <david@alley.com>
2023-03-27 12:08:48 -0400

    WIP

---
+++ src/rip7c/Boolean.php
@@ -0,0 +1,20 @@
+<?php
+/*
+ * This file is part of dlh01/rip7c.
+ *
+ * (c) David Herrera <mail@dlh01.info>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace rip7c;
+
+interface Boolean
+{
+	public function isTrue(callable $then, $else);
+
+	public function isFalse(callable $then, $else);
+}
