39e1c17e3caae0afd245c79f3366a990d0fe5847
David Herrera <david@alley.com>
2023-03-27 12:36:14 -0400

    WIP

---
+++ src/rip7c/Result.php
@@ -0,0 +1,51 @@
+<?php
+/*
+ * This file is part of dlh01/rip7c.
+ *
+ * (c) David Herrera <mail@dlh01.info>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace rip7c;
+
+use Alley\Validator\Type;
+use Laminas\Validator\ValidatorInterface;
+
+final class Result implements Boolean
+{
+    private ValidatorInterface $test;
+
+    /**
+     * @var callable
+     */
+    private $value;
+
+    public function __construct(
+        ValidatorInterface $test,
+        callable $value,
+    ) {
+        $this->test  = $test;
+        $this->value = $value;
+    }
+
+    public static function create(ValidatorInterface $test, $value): Result
+    {
+        $cls = fn() => $value;
+        $rst = new self(new Type(['type' => 'callable']), $cls);
+        return new self($test, $rst->isTrue($cls, $cls));
+    }
+
+    public function isTrue(callable $then, $else)
+    {
+        return $this->test->isValid(($this->value)()) ? $then() : $else;
+    }
+
+    public function isFalse(callable $then, $else)
+    {
+        return $this->test->isValid(($this->value)()) ? $else : $then();
+    }
+}
---
+++ tests/rip7c/ResultTest.php
@@ -0,0 +1,33 @@
+<?php
+/*
+ * This file is part of dlh01/rip7c.
+ *
+ * (c) David Herrera <mail@dlh01.info>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace rip7c;
+
+use Alley\Validator\AlwaysValid;
+use Alley\Validator\Not;
+
+final class ResultTest extends \PHPUnit\Framework\TestCase
+{
+    public function testIsTrue()
+    {
+        $expected = 'foo';
+        $result = Result::create(new AlwaysValid(), 'bar');
+        $this->assertSame($expected, $result->isTrue(fn() => $expected, 'baz'));
+    }
+
+    public function testIsFalse()
+    {
+        $expected = 'foo';
+        $result = Result::create(new Not(new AlwaysValid(), 'test'), 'bar');
+        $this->assertSame($expected, $result->isTrue(fn() => 'baz', $expected));
+    }
+}
